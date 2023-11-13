1c2fdcff049c819f31f39ff0b1891c970add378b
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-13 20:22:04 +0600

    Triple JS

---
+++ triple.js
@@ -0,0 +1,16 @@
+// always use ===
+// equal comparison doesnt work non primitive type
+
+const first = [2, 3, 4];
+const second = [34, 21, 3];
+
+if(first == second){
+    console.log('Values are equal')
+}
+else{
+    console.log('Values are not equal')
+}
+
+
+// type corecion
+// type conversion
